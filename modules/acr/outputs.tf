5a131356b2e962daced12a5dc366119bd4ce9722
ShellyDekel <shellydekel2003@gmail.com>
2024-03-03 18:00:50 +0200

    refactored output descriptions for the ACR, added dns and object outputs.

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -1,40 +1,50 @@
-output "acr_id" {
-  description = "string, the id of the acr"
+output "id" {
+  description = "The id of the acr."
   value       = azurerm_container_registry.container_registry.id
 }
 
-output "acr_object" {
-  description = "string, the id of the acr"
+output "name" {
+  description = "The name of the acr."
+  value       = azurerm_container_registry.container_registry.name
+}
+
+output "object" {
+  description = "The acr object."
   value       = azurerm_container_registry.container_registry
 }
 
-output "acr_name" {
-  value       = azurerm_container_registry.container_registry.name
-  description = "string, the name of the acr"
-}
-
-output "acr_login_server" {
-  value       = azurerm_container_registry.container_registry.login_server
-  description = "string, the login server of the acr"
-}
-
-output "acr_admin_username" {
+output "admin_username" {
   value       = azurerm_container_registry.container_registry.admin_username
-  description = "string, the acr admin username"
+  description = "The acr admin username."
 }
 
-output "acr_admin_password" {
+output "admin_password" {
+  description = "The acr admin password."
   value       = azurerm_container_registry.container_registry.admin_password
   sensitive   = true
-  description = "string, sensitive, the acr admin password"
 }
 
 output "private_endpoint_id" {
+  description = "The id of the private endpoint."
   value       = azurerm_private_endpoint.acr_private_endpoint.id
-  description = "string, the id of the private endpoint"
 }
 
 output "private_endpoint_name" {
+  description = "The name of the private endpoint."
   value       = azurerm_private_endpoint.acr_private_endpoint.name
-  description = "tring, he name of the private endpoint"
+}
+
+output "private_endpoint" {
+  description = "The private endpoint object."
+  value       = azurerm_private_endpoint.acr_private_endpoint
+}
+
+output "private_dns_zone_id" {
+  description = "The id of the private dns zone."
+  value       = azurerm_private_dns_zone.acr_private_dns_zone.id
+}
+
+output "private_dns_zone" {
+  description = "The private dns zone object."
+  value       = azurerm_private_dns_zone.acr_private_dns_zone
 }
