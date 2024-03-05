bd98f225d9c0db2f4c5749e8d3a1365875023296
ShellyDekel <shellydekel2003@gmail.com>
2024-03-05 16:16:43 +0200

    added output for the firewall's private ip

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -1,38 +1,39 @@
 output "id" {
-  description = "the id of the firewall"
+  description = "The ID of the Firewall."
   value       = azurerm_firewall.firewall.id
 }
 
 output "name" {
-  description = "the name of the firewall"
+  description = "The name of the Firewall."
   value       = azurerm_firewall.firewall.name
 }
 
-output "private_ip_ranges" {
-  description = "the private ip ranges of the firewall"
-  value       = azurerm_firewall.firewall.private_ip_ranges
+output "private_ip_address" {
+  description = "The private IP address of the Firewall."
+  value       = azurerm_firewall.firewall.ip_configuration[0].private_ip_address
 }
 
 output "firewall_object" {
-  description = "the firewall object"
+  description = "The Firewall object."
   value       = azurerm_firewall.firewall
 }
 
 output "public_ip_id" {
-  description = "the id of the public ip of the firewall"
+  description = "The ID of the Public IP of the Firewall."
   value       = azurerm_public_ip.firewall_ip.id
 }
 
 output "management_ip_id" {
-  description = "the id of the management ip of the firewall"
+  description = "The ID of the Management IP of the Firewall."
   value       = azurerm_public_ip.firewall_management_ip.id
 }
 
 output "policy_id" {
-  description = "the id of the firewall policy"
+  description = "The ID of the Firewall Policy."
   value       = azurerm_firewall_policy.firewall_policy.id
 }
 
 output "policy_object" {
+  description = "The Firewall Policy object."
   value = azurerm_firewall_policy.firewall_policy
 }
