2128e139d5f36643d92c71d7b275d6b77050934c
ShellyDekel <shellydekel2003@gmail.com>
2024-03-05 16:55:32 +0200

    fixed descriptions

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,73 +1,73 @@
 variable "resource_group_name" {
   type        = string
-  description = "(Required) the name of the resource group"
+  description = "(Required) The name of the Resource Group."
 }
 
 variable "location" {
   type        = string
-  description = "(Required) the location of the resource"
+  description = "(Required) The location of the resource."
 }
 
 
 variable "firewall_name" {
   type        = string
-  description = "(Required) the name of the firewall."
+  description = "(Required) The name of the firewall."
 }
 
 variable "firewall_sku_name" {
   type        = string
   default     = "AZFW_VNet"
-  description = "(Optional) SKU name of the Firewall. Possible values are AZFW_Hub and AZFW_VNet. Defaults to AZFW_VNet."
+  description = "(Optional) The SKU name of the Firewall. Possible values are AZFW_Hub and AZFW_VNet. Defaults to AZFW_VNet."
 }
 
 variable "firewall_sku_tier" {
   type        = string
   default     = "Standard"
-  description = "(Optional) the SKU tier of the firewall, possible values are Premium, Standard and Basic. Defaults to Standard."
+  description = "(Optional) The SKU tier of the firewall. Possible values are Premium, Standard and Basic. Defaults to Standard."
 }
 
 variable "ip_sku_tier" {
   type        = string
   default     = "Standard"
-  description = "(Optional) the SKU tier of the firewall's public IP's, Possible values are Standard and Basic. Defaults to Standard."
+  description = "(Optional) The SKU tier of the firewall's public IP's. Possible values are Standard and Basic. Defaults to Standard."
 }
 
 variable "network_rule_collection_group_name" {
   type        = string
   default     = "DefaultNetworkRuleCollectionGroup"
-  description = "(Optional) the name of the default network rule collection group."
+  description = "(Optional) The name of the default Network Rule Collection Group."
 }
 
 variable "network_rule_collection_name" {
   type        = string
   default     = "DefaultNetworkRuleCollection"
-  description = "(Optional) the name of the default network rule collection."
+  description = "(Optional) The name of the default Network Rule Collection."
 }
 
 variable "network_rules" {
   type = list(object({
     name                  = string
-    source_addresses      = list(string)
-    source_ip_groups      = list(string)
-    destination_addresses = list(string)
-    destination_fqdns     = list(string)
-    destination_ports     = list(string)
-    destination_ip_groups = list(string)
+    source_addresses      = optional(list(string))
+    source_ip_groups      = optional(list(string))
+    destination_addresses = optional(list(string))
+    destination_fqdns     = optional(list(string))
+    destination_ports     = optional(list(string))
+    destination_ip_groups = optional(list(string))
     protocols             = list(string)
   }))
   default = []
 
-  description = "(Optional) a list of network rules. the network rule format is: {name, source_addresses/ip_groups, destination_addresses/fqdns/ip_groups, destination_ports, protocols}."
+  description = "(Optional) A list of Network Rules."
 }
 
 variable "application_rules" {
   type = list(object({
     name                  = string
-    source_addresses      = list(string)
-    source_ip_groups      = list(string)
-    destination_addresses = list(string)
-    destination_fqdns     = list(string)
-    destination_urls      = list(string)
+    source_addresses      = optional(list(string))
+    source_ip_groups      = optional(list(string))
+    destination_addresses = optional(list(string))
+    destination_fqdns     = optional(list(string))
+    destination_urls      = optional(list(string))
 
     protocols = map(string)
 
@@ -75,65 +75,65 @@ variable "application_rules" {
   }))
   default = []
 
-  description = "(Optional) a list of application rules. the application rule format is: {name, source_addresses/ip_groups, destination_addresses/fqdns/urls, protocols: {port = type (Http, Https, or Mssql)}, http_headers: {name = value}}"
+  description = "(Optional) A list of Application Rules."
 }
 
 variable "application_rule_collection_group_name" {
   type        = string
   default     = "DefaultApplicationRuleCollectionGroup"
-  description = "(Optional) the name of the default application rule collection group."
+  description = "(Optional) The name of the default Application Rule Collection Group."
 }
 
 variable "application_rule_collection_name" {
   type        = string
   default     = "DefaultApplicationRuleCollection"
-  description = "(Optional) the name of the default application rule collection."
+  description = "(Optional) The name of the default Application Rule Collection."
 }
 
 variable "nat_rule_collection_group_name" {
   type        = string
   default     = "DefaultNatRuleCollectionGroup"
-  description = "(Optional) the name of the default nat rule collection group."
+  description = "(Optional) The name of the default NAT Rule Collection Group."
 }
 
 variable "nat_rule_collection_name" {
   type        = string
   default     = "DefaultNatRuleCollection"
-  description = "(Optional) the name of the default nat rule collection."
+  description = "(Optional) The name of the default NAT Rule Collection."
 }
 
 variable "nat_rules" {
   type = list(object({
     name                = string
-    source_addresses    = list(string)
-    source_ip_groups    = list(string)
-    destination_address = list(string)
-    destination_ports   = list(string)
+    source_addresses    = optional(list(string))
+    source_ip_groups    = optional(list(string))
+    destination_address = optional(list(string))
+    destination_ports   = optional(list(string))
     protocols           = list(string)
     translated_port     = list(string)
   }))
   default = []
 
-  description = "(Optional) a list of nat rules. the nat rule format is: {name, source_addresses/ip_groups, destination_address, destination_ports, protocols, translated port}."
+  description = "(Optional) A list of NAT Rules."
 }
 
 variable "log_analytics_workspace_id" {
   type        = string
   default     = null
-  description = "(Optional) link resource to a log analytics workspace to enable logs."
+  description = "(Optional) Link the resource to a Log Analytics Workspace to enable logs."
 }
 
 variable "virtual_network_name" {
   type        = string
-  description = "(Required) the name of the virtual network the firewall will sit on"
+  description = "(Required) The name of the Virtual Network the Firewall will sit on."
 }
 
 variable "firewall_subnet_id" {
   type        = string
-  description = "(Required) the id of the firewall subnet"
+  description = "(Required) The ID of the Firewall Subnet"
 }
 
 variable "firewall_management_subnet_id" {
   type        = string
-  description = "(Required) the id of the firewall management subnet"
+  description = "(Required) The ID of the Firewall Management Subnet."
 }
