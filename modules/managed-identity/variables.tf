1f23ce4cb7161cc7c1c99dc79a1c25e6ad3bd662
ShellyDekel <shellydekel2003@gmail.com>
2024-03-10 11:44:27 +0200

    minor change to description

--- modules/managed-identity/variables.tf
+++ modules/managed-identity/variables.tf
@@ -5,7 +5,7 @@ variable "resource_group_name" {
 
 variable "location" {
   type        = string
-  description = "(Required) the location of the resource."
+  description = "(Required) The location of the resource."
 }
 
 variable "name" {
